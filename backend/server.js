7114bf5f6dc3d442b1b68b1b8225441a33ab054e
Tim B <tbatty7@gmail.com>
2019-08-03 13:23:42 -0400

    added endpoint to server to create account

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,17 @@ connection.once('open', () => {
     console.log('MongoDB connection established successfully');
 });
 
-
+// endpoint to create new user account
+router.route('/account/create').post((req, res) => {
+    let account = new Account(req.body);
+    account.save()
+        .then(account => {
+            res.status(200).json({'message': 'Account Created Successfully!'});
+        })
+        .catch(err => {
+            res.status(400).send('Failed to create account');
+        });
+});
 
 app.use('/', router);
 
