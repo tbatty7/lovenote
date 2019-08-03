e9a34feab354af90da5c3380ad2b8b41c44783dc
Tim B <tbatty7@gmail.com>
2019-08-03 15:19:38 -0400

    added method in service to create account by calling backend

--- frontend/src/app/account.service.ts
+++ frontend/src/app/account.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor() { }
+  url = 'http://localhost:4000';
+  constructor(private http: HttpClient) { }
+  createAccount(user, passwrd) {
+    const newAccount = {
+      name: user,
+      password: passwrd
+    };
+    return this.http.post(`${this.url}/account/create`, newAccount);
+  }
+
 }
