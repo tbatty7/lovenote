b6851a2609d9082232957ecaf7b35e6560770ad8
Tim B <tbatty7@gmail.com>
2019-08-03 13:22:34 -0400

    wired up mongoDB successfully

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,23 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
+import Account from './models/Account';
+
 const app = express();
 const router = express.Router();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('');
+mongoose.connect('mongodb://localhost:27017/lovenote');
 
 const connection = mongoose.connection;
 
 connection.once('open', () => {
     console.log('MongoDB connection established successfully');
 });
+
+
 
 app.use('/', router);
 
