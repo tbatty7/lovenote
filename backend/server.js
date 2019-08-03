f7ad3dbde34ecdc5e492ecda24c49000f9103c25
Tim B <tbatty7@gmail.com>
2019-08-03 09:44:35 -0400

    added plumbing for mongoose to connect to MongoDB in express server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,22 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
 const app = express();
-app.get('/', (req, res) => res.send('Hello Lovenote!') );
+const router = express.Router();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('');
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('MongoDB connection established successfully');
+});
+
+app.use('/', router);
+
 app.listen(4000, () => console.log('Express server is running on port 4000'));
