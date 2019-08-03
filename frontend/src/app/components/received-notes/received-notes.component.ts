cb84e1e2b7fefa54a3edf7ac8a5a57f6eb0c7e1a
Tim B <tbatty7@gmail.com>
2019-08-03 15:52:25 -0400

    added getting the account in receivedNotes component successfully

--- frontend/src/app/components/received-notes/received-notes.component.ts
+++ frontend/src/app/components/received-notes/received-notes.component.ts
@@ -7,10 +7,13 @@ import { AccountService} from '../../account.service';
   styleUrls: ['./received-notes.component.css']
 })
 export class ReceivedNotesComponent implements OnInit {
-
+  id = '5d45cca4bd3d86307cb17d5a';
   constructor(private accountService: AccountService) { }
 
   ngOnInit() {
+    this.accountService.getAccount(this.id).subscribe((account) => {
+      console.log(account);
+    });
   }
 
 }
