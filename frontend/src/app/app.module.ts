dc612032348a12eee7a3039b3af3084e91a9880f
Tim B <tbatty7@gmail.com>
2019-08-14 22:19:41 -0400

    wired up write-lovenote component with a route

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,19 +5,18 @@ import { HttpClientModule} from '@angular/common/http';
 import { ReactiveFormsModule} from '@angular/forms';
 
 import { MatToolbarModule, MatFormFieldModule, MatInputModule, MatOptionModule, MatSelectModule, MatIconModule, MatTableModule, MatButtonModule, MatCardModule, MatDividerModule, MatSnackBarModule} from '@angular/material';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
 import { LoginFailureComponent } from './components/login-failure/login-failure.component';
 import { CreateAccountComponent } from './components/create-account/create-account.component';
 import { ReceivedNotesComponent } from './components/received-notes/received-notes.component';
-
-import { AccountService} from './account.service';
 import { LovedOnesComponent } from './components/loved-ones/loved-ones.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { WriteLovenoteComponent } from './components/write-lovenote/write-lovenote.component';
+
+import { AccountService} from './account.service';
 
 const routes: Routes = [
   { path: 'create', component: CreateAccountComponent },
@@ -26,6 +25,7 @@ const routes: Routes = [
   { path: 'received-notes/:id', component: ReceivedNotesComponent },
   { path: 'loved-ones/:id', component: LovedOnesComponent },
   { path: 'not-found/:id', component: NotFoundComponent },
+  { path: 'write-lovenote/:id', component: WriteLovenoteComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
