4c887ac371ba9ac23a1a435c66c9db3dad92d41b
Tim B <tbatty7@gmail.com>
2019-08-18 00:55:19 -0400

    added buttons on authored notes page and title

--- frontend/src/app/components/authored-notes/authored-notes.component.ts
+++ frontend/src/app/components/authored-notes/authored-notes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AccountService} from '../../account.service';
+import {Account} from '../../account.model';
 
 @Component({
   selector: 'app-authored-notes',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthoredNotesComponent implements OnInit {
 
-  constructor() { }
+  id;
+  myAccount: any = {};
+  constructor(private route: ActivatedRoute, private accountService: AccountService, private router: Router) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = params.id;
+      this.accountService.getAccount(this.id).subscribe((account: any) => {
+        this.myAccount = account;
+        this.getMyNotes();
+      });
+    });
   }
 
+  private getMyNotes() {
+    console.log('implement getMyNotes() method');
+  }
+
+  private toNotes() {
+    this.router.navigate([`/received-notes/${this.id}`]);
+  }
 }
