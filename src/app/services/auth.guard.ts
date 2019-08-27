fb0cf495ad3fb0333e7c288faae67759650b05a9
Tim B <tbatty7@gmail.com>
2019-08-27 16:05:19 -0400

    added auth guard that lets everything through

---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import {Router} from '@angular/router';
+import {AccountService} from './account.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return true;
+  }
+}
